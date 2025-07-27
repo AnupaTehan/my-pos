9cb816d52f852ac8d5ae404b4fe9aac49ab511c3
Anupa Tehan <anupatehan@gmail.com>
2025-07-27 15:28:12 +0530

    code base updated

--- src/main/java/controller/DashboardFormController.java
+++ src/main/java/controller/DashboardFormController.java
@@ -1,4 +1,23 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class DashboardFormController {
+
+    @FXML
+    void btnItemManagementOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnPurchaseOrderOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSupplierManagementOnAction(ActionEvent event) {
+
+    }
+
 }
